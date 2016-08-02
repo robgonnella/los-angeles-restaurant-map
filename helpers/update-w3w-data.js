b0c7ef2fe0971ed1432e6ea205ad926f8d51fd5d
Rob Gonnella <robgonnella@gmail.com>
2016-08-01 19:01:57 -0700

    update w3w script

--- helpers/update-w3w-data.js
+++ helpers/update-w3w-data.js
@@ -1,38 +1,56 @@
+var mongoose = require('../config/database')
 var Restaurant = require("../models/restaurant");
 var async = require('async');
 var rp = require('request-promise');
 require('../server');
 
-module.exports = (function() {
-  var apiUri = "https://api.what3words.com/v2/reverse"
+function getW3ws(rs, wcb) {
+  var baseUri = "https://api.what3words.com/v2/reverse"
   var keyParam = "&key=" + process.env.W3W_KEY;
-
-  //accesses what 3 words api and populates w3w field for all restaurants in database
-  Restaurant.find({}, function(err, restaurants){
-    if(err) return console.log("Error retrieving restaurants from database", err);
-    async.each(restaurants, function(r, acb){
-      if(r.w3w) {
+  async.each(rs, function(r, acb){
+    if(r.w3w) {
+      acb();
+    } else {
+      var coordsParam = `coords=${r.lat},${r.lon}`
+      var url = baseUri + "?" + coordsParam + keyParam;
+      rp(url, function(err, data){
+        if(err) return acb(err);
+        try {
+          data = JSON.parse(data.body)
+        }
+        catch(e) {
+          console.log(e.stack);
+        }
+        r.w3w = data.words
+        r.save();
+        console.log(`updated ${r.name} w3w to ${r.w3w}`)
         acb();
-      } else {
-        var coordsParam = `coords=${r.lat},${r.lon}`
-        var url = apiUri + "?" + coordsParam + keyParam;
-        rp(url, function(err, data){
-          try {
-            data = JSON.parse(data.body)
-          }
-          catch(e) {
-            console.log(e.stack);
-          }
-          r.w3w = data.words
-          r.save();
-          console.log(`updated ${r.name} w3w to ${r.w3w}`)
-          acb();
-        })
-      }
-    }, function(err){
-      if(err) return console.log(err)
-      console.log("successfully updated all w3w's!")
-      process.exit();
-    })
+      })
+    }
+  }, function(err){
+    if(err) return wcb(err);
+    wcb(null, "successfully updated all w3w's!")
   })
-})();
+}
+
+function getAllRestaurants(wcb) {
+  Restaurant.find({}, function(err, rs){
+    if(err) return wcb(err);
+    wcb(null, rs)
+  });
+}
+
+module.exports = async.waterfall([
+  function(wcb) {
+    getAllRestaurants(wcb)
+  },
+
+  function(rs, wcb) {
+    getW3ws(rs, wcb);
+  }
+
+], function(err, result){
+  if(err) console.log(err);
+  if(result) console.log(result);
+  mongoose.disconnect();
+});
