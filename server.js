42260d677181d8b35a93d2d8e3847832bc777df4
Rob Gonnella <robgonnella@gmail.com>
2016-08-03 15:32:57 -0700

    add access control headers middleware in server.js

--- server.js
+++ server.js
@@ -28,6 +28,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin',  '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+
+  if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 // pass app to routers
 require('./routes/api-routes')(app);
 
