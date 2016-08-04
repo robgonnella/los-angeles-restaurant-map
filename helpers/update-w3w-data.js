0ff72abcadbc5f879be33acc92c4d7e3bea8d0df
Rob Gonnella <robgonnella@gmail.com>
2016-08-03 17:11:48 -0700

    change helper update-w3w-data.js to async.eachLimt to avoid ECCONRESET and socket hangup

--- helpers/update-w3w-data.js
+++ helpers/update-w3w-data.js
@@ -7,7 +7,7 @@ require('../server');
 function getW3ws(rs, wcb) {
   var baseUri = "https://api.what3words.com/v2/reverse"
   var keyParam = "&key=" + process.env.W3W_KEY;
-  async.each(rs, function(r, acb){
+  async.eachLimit(rs, 100, function(r, acb){
     if(r.w3w) {
       acb();
     } else {
